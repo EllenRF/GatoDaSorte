e114b043d207c5147890d011fcd79664f74fb842
Ellen Rocha <ellen.farias@modalgr.com.br>
2023-08-23 12:50:04 -0300

    configuração de cookie da the cat api

--- src/servicos/servicos.js
+++ src/servicos/servicos.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 // Utilizado para pegar as frases
-const sorteAPI = axios.create({ baseURL: "https://glitch.com/~gato-api"  })
+const sorteAPI = axios.create({ baseURL: "https://glitch.com/~gato-api" })
 async function getSortes() {
     const response = await sorteAPI.get('/')
     return response.data
@@ -9,11 +10,12 @@ async function getSortes() {
 
 // Utilizado para pegar as imagens
 async function getImagens() {
+    const csrfToken = Cookies.get('csrfToken');
     const apiKey = process.env.CAT_API_KEY;
     const url = "https://api.thecatapi.com/v1/images/search"
     sessionStorage.setItem("carregandoImagem", true);
 
-    axios.get(url, { headers: { "x-api-key": apiKey } })
+    axios.get(url, { withCredentials: true, headers: { "x-api-key": apiKey, "X-CSRF-TOKEN": csrfToken } })
         .then(function (response) {
             sessionStorage.setItem("url", response.data[0].url)
             sessionStorage.setItem("carregandoImagem", false);
