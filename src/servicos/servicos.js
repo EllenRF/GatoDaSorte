fc95b9bd2d549c1ae204f89582c1a941b878fe1d
Ellen Rocha <ellen.farias@modalgr.com.br>
2023-08-27 22:32:39 -0300

    modificacao servico.js

--- src/servicos/servicos.js
+++ src/servicos/servicos.js
@@ -11,12 +11,10 @@ async function getSortes() {
 // Utilizado para pegar as imagens
 async function getImagens() {
     const csrfToken = Cookies.get('csrfToken');
-    const apiKey = process.env.CAT_API_KEY;
-    alert(apiKey)
     const url = "https://api.thecatapi.com/v1/images/search"
     sessionStorage.setItem("carregandoImagem", true);
 
-    axios.get(url, { withCredentials: true, headers: { "x-api-key": apiKey, "X-CSRF-TOKEN": csrfToken, "Origin": 'https://ellenrf.github.io' } })
+    axios.get(url, { withCredentials: true, headers: { "x-api-key": process.env.CAT_API_KEY, "X-CSRF-TOKEN": csrfToken, "Origin": 'https://ellenrf.github.io' } })
         .then(function (response) {
             sessionStorage.setItem("url", response.data[0].url)
             sessionStorage.setItem("carregandoImagem", false);
